7903bc62d877ac132d9d5ca0f2fb8f4918687d61
mvs139 <118193509+mvs139@users.noreply.github.com>
2023-01-17 11:01:16 +0100

    [ADD] app pdm

---
+++ pdm/reports/mrp_report_bom_structure.py
@@ -0,0 +1,86 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    ServerPLM, Open Source Product Lifcycle Management System    
+#    Copyright (C) 2011-2015 OmniaSolutions srl (<http://www.omniasolutions.eu>). All Rights Reserved
+#    Copyright (C) 2016-2020 Techspell srl (<http://www.techspell.eu>). All Rights Reserved
+#    Copyright (C) 2020-2021 Didotech srl (<http://www.didotech.com>). All Rights Reserved
+#    
+#    Created on : 2018-03-01
+#    Author : Fabio Colognesi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import json
+
+from odoo import api, models, _
+from odoo.tools import float_round
+
+class ReportBomStructure(models.AbstractModel):
+    _inherit = 'report.mrp.report_bom_structure'
+
+    def _add_engineering_data(self, components):
+        for line in components:
+            line['prod_desc'] = ""
+            line['prod_revi'] = ""
+            line['prod_stat'] = ""
+            prod_id = line.get('prod_id')
+            if prod_id:
+                prod_id = self.env['product.product'].browse(prod_id)
+                line['prod_desc'] = prod_id.description
+                line['prod_revi'] = prod_id.engineering_revision
+                line['prod_stat'] = prod_id.state
+
+        return True
+
+    def _add_engineering_void_data(self, operations):
+        """
+            Maintains coherence column showing data.
+        """
+        for line in operations:
+            line['desc'] = ""
+            line['revi'] = ""
+            line['stat'] = ""
+        return operations
+   
+### OVERRIDDEN STANDARD METHODS 
+
+    def _get_bom_lines(self, bom, bom_quantity, product, line_id, level):
+        components, total = super(ReportBomStructure, self)._get_bom_lines(bom, bom_quantity, product, line_id, level)
+        self._add_engineering_data(components)
+        return components, total
+
+    def _get_pdf_line(self, bom_id, product_id=False, qty=1, unfolded_ids=None, unfolded=False):
+        data = super(ReportBomStructure, self)._get_pdf_line(bom_id, product_id, qty, unfolded_ids, unfolded)
+        self._add_engineering_data(data['lines'])
+        return data
+
+    def _get_operation_line(self, product, bom, qty, level, index):
+        data = super(ReportBomStructure, self)._get_operation_line(product, bom, qty, level, index)
+        self._add_engineering_void_data(data)
+        return data
+ 
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        data['childs']=False
+        data['quantity']=1
+        if data.get('unfolded_ids'):
+            if not list(set(json.loads(data.get('unfolded_ids')))):
+                data.pop('unfolded_ids')
+        return super(ReportBomStructure, self)._get_report_values(docids, data)
+
+### OVERRIDDEN STANDARD METHODS 
+
